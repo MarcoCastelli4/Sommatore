41aa735d0bbbb93c879d40fe22868eea0aff1622
Yuri Botticchio <yuribotticchio@gmail.com>
2018-02-19 09:01:10 +0100

    Aggiunto somma10

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -5,14 +5,22 @@ public class Sommatore
 	{
 		for (int i = 0; i < 4; i++) 
 			x++;
-			return x;
+		return x;
 	}
+	
+	public static int somma10(int x)
+	{
+		for (int i = 0; i < 10; i++) 
+			x++;
+		return x;
+	}
+	
 	
 	public static void main(String[] args) 
 	{
 		int x=1;
 		System.out.println(somma4(x));
-
+		System.out.println(somma10(x));
 	}
 	
 	
